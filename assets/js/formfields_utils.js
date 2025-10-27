120a9936568aab9617c1a629b494d1a838673538
Adeilton Malafaia dos Santos <adeiltonmalafaia@gmail.com>
2025-10-26 23:51:45 -0300

    Implementing function to center an element relative to its parent

--- assets/js/formfields_utils.js
+++ assets/js/formfields_utils.js
@@ -1,3 +1,22 @@
+// Centering an element relative to its parent
+function centerElement(element){
+    // Getting element dimensions
+    const elementWidth = element.offsetWidth;
+    const elementHeight = element.offsetHeight;
+
+    // Getting parent element dimensions
+    const parentWidth = element.parentElement.offsetWidth;
+    const parentHeight = element.parentElement.offsetHeight;
+
+    // Calculating top and left postions element to centering relative to its parent
+    const top = (parentHeight - elementHeight) / 2;
+    const left = (parentWidth - elementWidth) / 2;
+
+    // Positioning element
+    element.style.top = `${top}px`;
+    element.style.left = `${left}px`;
+}
+
 // Apply mask in value of phone field
 function maskPhone(idPhoneField, maxlengthPhoneField){
     const phone = document.getElementById(idPhoneField);
